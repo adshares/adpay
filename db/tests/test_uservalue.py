f1da98fd3a8c1baf7f3cac2502434d2451c41232
Krzysztof Kuchta <kkuchta@togetherdata.com>
2017-11-23 16:54:38 +0100

    Adding user values db tests.

--- db/tests/test_uservalue.py
+++ db/tests/test_uservalue.py
@@ -19,7 +19,7 @@ class DBTestCase(unittest.TestCase):
 
     @defer.inlineCallbacks
     def test_user_value(self):
-
+        # Test adding user value
         for i in range(100):
             yield db_utils.update_user_value(campaign_id="campaign_id",
                                              timestamp=3500,
@@ -27,3 +27,26 @@ class DBTestCase(unittest.TestCase):
                                              payment=i*10,
                                              human_score=10)
 
+        _iter = yield db_utils.get_user_value_iter("campaign_id", 3600)
+        user_values = []
+        while True:
+            user_value_doc = yield _iter.next()
+            if user_value_doc is None:
+                break
+            user_values.append(user_value_doc)
+            self.assertEqual(user_value_doc['human_score'], 10)
+            self.assertEqual(user_value_doc['payment'], user_value_doc['user_id']*10)
+        self.assertEqual(len(user_values), 100)
+
+        # Test update user value
+        for i in range(100):
+            yield db_utils.update_user_value(campaign_id="campaign_id",
+                                             timestamp=3500,
+                                             user_id=i,
+                                             payment=i*20,
+                                             human_score=10)
+
+        # Test get user value.
+        for i in range(100):
+            user_value_doc = yield db_utils.get_user_value("campaign_id", 3600, i)
+            self.assertEqual(user_value_doc["payment"], i*20)
