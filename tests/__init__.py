77427a049905ef401d846b80499a1991212e8b86
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-04-18 16:03:18 +0200

    Move tests to their own directory.

---
+++ tests/__init__.py
@@ -0,0 +1,95 @@
+import json
+
+from twisted.trial import unittest
+from twisted.internet import defer, reactor
+from twisted.internet.protocol import Protocol
+from twisted.web.client import Agent
+from twisted.internet.defer import succeed
+from twisted.web.iweb import IBodyProducer
+from twisted.web.http_headers import Headers
+
+from zope.interface import implements
+
+from adpay.iface import server as iface_server
+from adpay.iface import consts as iface_consts
+from adpay import db
+
+
+class DBTestCase(unittest.TestCase):
+    @defer.inlineCallbacks
+    def setUp(self):
+        self.conn = yield db.get_mongo_connection()
+        self.db = yield db.get_mongo_db()
+        yield db.configure_db()
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self.conn.drop_database(self.db)
+        yield db.disconnect()
+
+
+class StringProducer(object):
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
+
+
+class ReceiverProtocol(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.body = []
+
+    def dataReceived(self, databytes):
+        self.body.append(databytes)
+
+    def connectionLost(self, reason):
+        self.finished.callback(''.join(self.body))
+
+
+class IfaceTestCase(unittest.TestCase):
+    @defer.inlineCallbacks
+    def setUp(self):
+        self.conn = yield db.get_mongo_connection()
+        self.db = yield db.get_mongo_db()
+        yield db.configure_db()
+
+        self.port = iface_server.configure_iface()
+        self.client = Agent(reactor)
+
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self.conn.drop_database(self.db)
+        yield db.disconnect()
+
+        self.port.stopListening()
+
+    @defer.inlineCallbacks
+    def get_response(self, method, params=None):
+        post_data = StringProducer(json.dumps({
+            "jsonrpc": "2.0",
+            "id": "test_hit",
+            "method": method,
+            "params": params
+        }))
+
+        response = yield self.client.request('POST',
+                                             'http://127.0.0.1:%s' % iface_consts.SERVER_PORT,
+                                             Headers({'content-type': ['text/plain']}),
+                                             post_data)
+
+        finished = defer.Deferred()
+        response.deliverBody(ReceiverProtocol(finished))
+        data = yield finished
+        defer.returnValue(json.loads(data) if data else None)
