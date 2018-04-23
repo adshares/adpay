68a04a76d9dab0cff10c2a39bbe45780389d6cf7
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-04-23 17:34:47 +0200

    Send coffee.

--- tests/test_stats_const.py
+++ tests/test_stats_const.py
@@ -8,4 +8,4 @@ class TestSetConst(TestCase):
     def test_const(self):
 
         self.assertIsNot(stats_const.SECONDS_PER_HOUR, None)
-        self.assertEqual(stats_const.MAX_USER_KEYWORDS_IN_PROFILE, None)
+        self.assertIsNot(stats_const.MAX_USER_KEYWORDS_IN_PROFILE, None)
