4e83b39df9b8fa2bdb7941ba71a354f2277c2a07
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-04-23 17:23:07 +0200

    Test fix.

--- tests/test_stats_const.py
+++ tests/test_stats_const.py
@@ -7,4 +7,5 @@ class TestSetConst(TestCase):
 
     def test_const(self):
 
-        self.assertIsNot(stats_const.SERVER_PORT, None)
+        self.assertIsNot(stats_const.SECONDS_PER_HOUR, None)
+        self.assertEqual(stats_const.MAX_USER_KEYWORDS_IN_PROFILE, None)
