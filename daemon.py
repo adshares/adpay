1e552cd5ef77b208a41345a410e96f91e12183af
Adam Włodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-05-16 15:21:32 +0200

    Initialize logging.

--- daemon.py
+++ daemon.py
@@ -1,15 +1,23 @@
+import logging.config
+import json
+import os
+
 from twisted.internet import reactor
-from twisted.python import log
-import sys
 
 from adpay.iface import server as iface_server
 from adpay.stats import tasks as stats_tasks
 from adpay import db
 
-log.startLogging(sys.stdout)
-
 
 if __name__ == "__main__":
+
+    logging.basicConfig()
+
+    logfile_path = os.path.join(os.environ["ADPAY_ROOT"], "adpay", "config", "log_config.json")
+
+    with open(logfile_path, "r") as fd:
+        logging.config.dictConfig(json.load(fd))
+
     db.configure_db()
     iface_server.configure_iface()
     stats_tasks.configure_tasks()
