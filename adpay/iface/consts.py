640c8e3e567ebdcda48b781e307546814db33b90
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-10-17 22:33:15 +0200

    travis debugging

--- adpay/iface/consts.py
+++ adpay/iface/consts.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(raise_error_if_not_found=True))
 
 print(os.environ)
+
+print(dotenv.find_dotenv())
+print(os.getcwd())
 
 #: Twisted TCP port number
 SERVER_PORT = int(os.getenv('ADPAY_SERVER_PORT'))
