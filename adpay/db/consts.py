ca084891fba3445ed959492e1bd1a37504f1f619
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-12 16:37:45 +0100

    add custom mongo host

--- adpay/db/consts.py
+++ adpay/db/consts.py
@@ -1,8 +1,9 @@
 import os
 
 #: MongoDB port
-MONGO_DB_PORT = int(os.getenv('ADPAY_MONGO_DB_PORT'))
-MONGO_DB_NAME = os.getenv('ADPAY_MONGO_DB_NAME')
+MONGO_DB_PORT = int(os.getenv('ADPAY_MONGO_DB_PORT', '27017'))
+MONGO_DB_NAME = os.getenv('ADPAY_MONGO_DB_NAME', 'adpay')
+MONGO_DB_HOST = os.getenv('ADPAY_MONGO_DB_HOST', 'localhost')
 
 #: Click event
 EVENT_TYPE_CLICK = os.getenv('ADPAY_EVENT_TYPE_CLICK')
