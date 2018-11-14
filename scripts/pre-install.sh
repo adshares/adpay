579de2bae5e82463879ff318661911f5175430d3
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-11-14 19:26:32 +0100

    --no-install-recommends

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -5,6 +5,6 @@ set -e
 # Ubuntu 18.04 only
 
 # Install dependencies for python operations
-apt-get -qq -y install python python-pip
+apt-get -qq -y install --no-install-recommends python python-pip
 
 pip install pipenv
