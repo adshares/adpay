c14c02d2b7d76add1b50d8b35d601beccf62ad3a
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-12-20 12:46:45 +0100

    refactoring

--- features/environment.py
+++ features/environment.py
@@ -1,27 +1,41 @@
 # -- FILE: features/environment.py
-from behave import use_fixture
-from features.fixtures import *
 from twisted.internet import reactor, defer
 from tests import DBTestCase
+from adpay.iface.server import configure_iface
+from adpay import db
+from tests import WebTestCase
 
 
 def before_tag(context, tag):
-    if tag == "fixture.adpay.server":
-        use_fixture(adpay_server, context)
+    if tag == "fixture.adpay.db":
+        context.apday_db_direct = True
 
 
 def before_scenario(context, scenario):
+    if context.apday_db_direct:
 
-    context.dtc = DBTestCase()
-    context.dfr = defer.Deferred()
-    context.dfr.addCallback(context.dtc.setUp)
+        context.dtc = DBTestCase()
+        context.dfr = defer.Deferred()
+        context.dfr.addCallback(context.dtc.setUp)
 
 
 def after_scenario(context, scenario):
+    if context.apday_db_direct:
 
-    context.dfr.addCallback(context.dtc.tearDown)
+        context.dfr.addCallback(context.dtc.tearDown)
+
+
+def before_all(context):
+    db.configure_db()
+
+    context.txserver = WebTestCase()
+    context.txserver.setUp()
+    #context.server = configure_iface()
+    #context.server_url = 'http://localhost:' + str(context.server.getHost().port)
+
+    context.apday_db_direct = False
 
 
 def after_all(context):
-    reactor.callLater(1, reactor.stop)
+    reactor.callLater(4, reactor.stop)
     reactor.run()
