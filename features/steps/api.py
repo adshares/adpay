a4a75cba3258101b15b66bd443ca9127dcfc69b5
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-12-20 12:46:29 +0100

    use twisted web client

--- features/steps/api.py
+++ features/steps/api.py
@@ -1,4 +1,5 @@
 from behave import *
+import json
 
 
 @given('I want to create or update a campaign using HTTP API')
@@ -33,3 +34,31 @@ def step_impl(context):
                        "params": []}
 
 
+@when('I provide the data')
+def step_impl(context):
+    context.request['params'] = json.loads(context.text)
+
+
+@when('I make request')
+def step_impl(context):
+    context.response = context.txserver.get_response(context.request['method'], context.request['params'])
+
+
+@then('The response should be "{code}"')
+def step_impl(context, code):
+    pass
+
+    #def test_code(response):
+    #    assert response.code == int(code)
+
+    #context.response.addCallback(test_code)
+
+
+@then('The response should contain')
+def step_impl(context):
+
+    def test_code(response, resp_text):
+        assert response == json.loads(resp_text)
+
+    resp_text = context.text
+    context.response.addCallback(test_code, resp_text)
