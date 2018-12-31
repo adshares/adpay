fafd39e4e295ec673643c5344f4755308c2916c3
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2018-12-31 17:20:08 +0100

    update tests

--- adpay/stats/consts.py
+++ adpay/stats/consts.py
@@ -5,3 +5,5 @@ SECONDS_PER_HOUR = int(os.getenv('ADPAY_SECONDS_PER_HOUR', 3600))
 
 #: Max keywords in a user profile
 MAX_USER_KEYWORDS_IN_PROFILE = int(os.getenv('ADPAY_MAX_USER_KEYWORDS_IN_PROFILE', 50))
+
+CALCULATION_METHOD = os.getenv('ADPAY_CALCULATION_METHOD', 'default')
