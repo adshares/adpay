c7048f24302147f971d2813d7aa8b0708ea89cb5
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2019-01-02 14:01:59 +0100

    Refactoring

--- adpay/iface/consts.py
+++ adpay/iface/consts.py
@@ -6,11 +6,9 @@ import os
 SERVER_PORT = int(os.getenv('ADPAY_SERVER_PORT', 9091))
 
 #: JSONRPC error code returned when payment round is not calculated yet
-#:
-#: `Environmental variable override: ADPAY_PAYMENTS_NOT_CALCULATED_YET`
 PAYMENTS_NOT_CALCULATED_YET = -32000
 
-#: Enable an endpoint, which allows to force payment recalculation. For development use only!
+#: Enable an endpoint, which allows to force payment recalculation. 1 for enable, 0 for disable. For development use only!
 #:
 #: `Environmental variable override: ADPAY_DEBUG_ENDPOINT`
 DEBUG_ENDPOINT = bool(os.getenv('ADPAY_DEBUG_ENDPOINT', 0))
