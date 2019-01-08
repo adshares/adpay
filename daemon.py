0b6a32f29b9b64642639fd195a7bea47c0eda6a2
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2019-01-08 14:59:25 +0100

    comments

--- daemon.py
+++ daemon.py
@@ -7,8 +7,17 @@ from adpay.utils import logs as server_logging
 
 if __name__ == "__main__":
 
+    # Set up logging.
     server_logging.setup()
+
+    # Configuring database.
     db.configure_db()
+
+    # Start http interface to communicate with others AdShares components.
     iface_server.configure_iface()
+
+    # Configure periodical tasks.
     stats_tasks.configure_tasks()
+
+    # Run.
     reactor.run()
