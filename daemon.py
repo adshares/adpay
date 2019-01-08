09a266131218157b949de538f60bc1d375287128
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2019-01-08 14:54:56 +0100

    return -32010 when receiving invalid objects

--- daemon.py
+++ daemon.py
@@ -1,23 +1,13 @@
-import logging.config
-import json
-import os
-
 from twisted.internet import reactor
 
+from adpay import db
 from adpay.iface import server as iface_server
 from adpay.stats import tasks as stats_tasks
-from adpay import db
-
+from adpay.utils import logs as server_logging
 
 if __name__ == "__main__":
 
-    logging.basicConfig()
-
-    logfile_path = os.path.join(os.getenv('ADPAY_LOG_CONFIG_FILE', 'config/log_config.json'))
-
-    with open(logfile_path, "r") as fd:
-        logging.config.dictConfig(json.load(fd))
-
+    server_logging.setup()
     db.configure_db()
     iface_server.configure_iface()
     stats_tasks.configure_tasks()
