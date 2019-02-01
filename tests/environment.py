ea4ddeff8ae6752aea322ec71a675969aace76dc
awlodarkiewicz <adam.wlodarkiewicz+github@gmail.com>
2019-02-01 10:50:38 +0100

    Mongomock updates

--- tests/environment.py
+++ tests/environment.py
@@ -5,9 +5,13 @@ from twisted.internet import reactor
 
 from adpay.iface.consts import SERVER_PORT
 from adpay.iface.server import configure_iface
+from tests import MongoMockTestCase
 
 
 def before_all(context):
+    # Mock Mongo
+    context.mock_db = MongoMockTestCase()
+    context.mock_db.setUp()
 
     # Configure our interface url
     host = socket.gethostbyname(socket.gethostname())
@@ -16,6 +20,8 @@ def before_all(context):
 
     # Start listening
     context.server = configure_iface(port=port)
+
+    # Run Twisted reactor
     Thread(target=reactor.run, args=(False,)).start()
 
 
@@ -23,4 +29,9 @@ def after_all(context):
 
     # Shutdown interface
     context.server.stopListening()
+
+    # Shutdown Twisted reactor
     reactor.stop()
+
+    # Shutdown mock Mongo
+    context.mock_db.tearDown()
