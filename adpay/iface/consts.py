ae980fdb5933e6115a4f0362278c18adea3c8524
Paweł Zaremba <pawel.zaremba@adshares.pl>
2019-02-07 16:32:11 +0100

    Update const

--- adpay/iface/consts.py
+++ adpay/iface/consts.py
@@ -3,7 +3,7 @@ import os
 #: Twisted TCP port number, ie. AdPay server port
 #:
 #: `Environmental variable override: ADPAY_SERVER_PORT`
-SERVER_PORT = int(os.getenv('ADPAY_SERVER_PORT', 9091))
+SERVER_PORT = int(os.getenv('ADPAY_SERVER_PORT', 8012))
 
 #: JSONRPC error code returned when payment round is not calculated yet
 PAYMENTS_NOT_CALCULATED_YET = -32000
@@ -14,4 +14,4 @@ INVALID_OBJECT = -32010
 #: Enable an endpoint, which allows to force payment recalculation. 1 for enable, 0 for disable. For development use only!
 #:
 #: `Environmental variable override: ADPAY_DEBUG_ENDPOINT`
-DEBUG_ENDPOINT = bool(int(os.getenv('ADPAY_DEBUG_ENDPOINT', 0)))
+DEBUG_ENDPOINT = bool(int(os.getenv('ADPAY_ENABLE_DEBUG_ENDPOINT', 0)))
