63759c47fb7142eba7eac2ed80bf2dc87b8d8522
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-10-14 14:57:01 +0200

    Test duplicate events

--- tests/UI/Controller/EventControllerTest.php
+++ tests/UI/Controller/EventControllerTest.php
@@ -189,4 +189,57 @@ final class EventControllerTest extends WebTestCase
         $client->request('POST', '/api/v1/events/conversions', [], [], [], json_encode($parameters));
         $this->assertEquals(422, $client->getResponse()->getStatusCode());
     }
+
+    public function testDuplicateEvents(): void
+    {
+        $parameters1 = [
+            'time_start' => 1231230,
+            'time_end' => 1231235,
+            'events' => [
+                [
+                    'id' => 'ffc567e1396b4cadb52223a51796fdf1',
+                    'time' => 1231231,
+                    'case_id' => '43c567e1396b4cadb52223a51796fd01',
+                    'publisher_id' => 'ffc567e1396b4cadb52223a51796fd02',
+                    'zone_id' => 'aac567e1396b4cadb52223a51796fdbb',
+                    'advertiser_id' => 'ccc567e1396b4cadb52223a51796fdcc',
+                    'campaign_id' => 'ddc567e1396b4cadb52223a51796fddd',
+                    'banner_id' => 'ddc567e1396b4cadb52223a51796fddd',
+                    'impression_id' => '13c567e1396b4cadb52223a51796fd03',
+                    'tracking_id' => '23c567e1396b4cadb52223a51796fd02',
+                    'user_id' => '33c567e1396b4cadb52223a51796fd01',
+                    'human_score' => 0.99,
+                ],
+            ],
+        ];
+
+        $client = static::createClient();
+
+        $client->request('POST', '/api/v1/events/views', [], [], [], json_encode($parameters1));
+        $this->assertEquals(204, $client->getResponse()->getStatusCode());
+
+        $parameters1 = [
+            'time_start' => 1231236,
+            'time_end' => 1231239,
+            'events' => [
+                [
+                    'id' => 'ffc567e1396b4cadb52223a51796fdf1',
+                    'time' => 1231237,
+                    'case_id' => '43c567e1396b4cadb52223a51796fd01',
+                    'publisher_id' => 'ffc567e1396b4cadb52223a51796fd02',
+                    'zone_id' => 'aac567e1396b4cadb52223a51796fdbb',
+                    'advertiser_id' => 'ccc567e1396b4cadb52223a51796fdcc',
+                    'campaign_id' => 'ddc567e1396b4cadb52223a51796fddd',
+                    'banner_id' => 'ddc567e1396b4cadb52223a51796fddd',
+                    'impression_id' => '13c567e1396b4cadb52223a51796fd03',
+                    'tracking_id' => '23c567e1396b4cadb52223a51796fd02',
+                    'user_id' => '33c567e1396b4cadb52223a51796fd01',
+                    'human_score' => 0.99,
+                ],
+            ],
+        ];
+
+        $client->request('POST', '/api/v1/events/views', [], [], [], json_encode($parameters1));
+        $this->assertEquals(422, $client->getResponse()->getStatusCode());
+    }
 }
