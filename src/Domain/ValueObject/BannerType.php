9590bc2cf46cf14834d52a15c906d26f77543daa
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-11-28 18:12:00 +0100

    Direct banners support

--- src/Domain/ValueObject/BannerType.php
+++ src/Domain/ValueObject/BannerType.php
@@ -10,12 +10,14 @@ final class BannerType
 
     public const HTML = 'html';
 
+    public const DIRECT = 'direct';
+
     /** @var string */
     private $type;
 
     public function __construct(string $type)
     {
-        if ($type !== self::IMAGE && $type !== self::HTML) {
+        if ($type !== self::IMAGE && $type !== self::HTML && $type !== self::DIRECT) {
             throw InvalidArgumentException::fromArgument('type', $type);
         }
 
