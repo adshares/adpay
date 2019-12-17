c1fa6f9c9aa53e711387ea3764a29f19eca62a60
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2019-12-17 12:10:51 +0100

    Small fixes

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,8 +3,7 @@
 [[ -z ${1:-""} ]] && set -eu || source ${1}/_functions.sh --vendor
 cd ${2:-"."}
 
-export APP_VERSION=$(versionFromGit 2>/dev/null || echo "")
-echo "=== Building v${APP_VERSION} of ${SERVICE_NAME} ==="
+echo "=== Building ${APP_VERSION} of ${SERVICE_NAME} ==="
 
 composer install --no-dev --optimize-autoloader
 php bin/console doctrine:migrations:migrate --no-interaction
